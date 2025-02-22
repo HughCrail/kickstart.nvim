40cc1f3f3c51c2b83568552e63f72c5d657d9450
Hugh Crail <hughcrail@gmail.com>
2025-02-22 15:07:34 +0000

    toggle copilot

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,9 @@ return {
   'tpope/vim-sleuth',
   {
     'github/copilot.vim',
+    init = function()
+      vim.g.copilot_enabled = true
+    end,
     config = function()
       vim.keymap.set('i', '<m-l>', 'copilot#AcceptWord("")', {
         expr = true,
@@ -10,6 +13,20 @@ return {
       vim.keymap.set('i', '<c-m-l>', 'copilot#AcceptLine("")', {
         expr = true,
         desc = 'Accept Copilot Line',
+      })
+      -- Toggle Copilot
+      vim.keymap.set('n', '<leader>tc', function()
+        if vim.g.copilot_enabled then
+          vim.cmd 'Copilot disable'
+          vim.g.copilot_enabled = false
+          vim.notify 'Copilot disabled'
+        else
+          vim.cmd 'Copilot enable'
+          vim.g.copilot_enabled = true
+          vim.notify 'Copilot enabled'
+        end
+      end, {
+        desc = 'Toggle Copilot',
       })
     end,
   },
