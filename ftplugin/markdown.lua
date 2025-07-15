a3b4fc1c429b5a4720073f501c704175d203808e
Hugh Crail <hughcrail@gmail.com>
2025-07-15 22:19:30 +0100

    markdown todo status

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -96,27 +96,77 @@ end
 local function mark_impl(cb)
   local bufnr = vim.api.nvim_get_current_buf()
   search_up(function(node)
-    if node:type() == 'list_item' and is_task_list(node) then
+    if node:type() == 'list_item' then
       local row = node:start()
       local line = vim.api.nvim_buf_get_lines(bufnr, row, row + 1, true)[1]
-      vim.api.nvim_buf_set_lines(bufnr, row, row + 1, true, { cb(line) })
+      local new_line = cb(line, is_task_list(node))
+      if line == new_line then
+        return true
+      end
+      vim.api.nvim_buf_set_lines(bufnr, row, row + 1, true, { new_line })
       return true
     end
   end, function() end)
 end
 
+local function remove_wait_tag(ln)
+  local s, _ = string.gsub(ln, ':wait:', '', 1)
+  return s
+end
+
+local function transform_todo(ln, is_task)
+  local s = ln
+  if is_task then
+    s, _ = string.gsub(s, '%[x%]', '[ ]', 1)
+  else
+    s, _ = string.gsub(s, '^(%s*%-)(.*)', '%1 [ ]%2', 1)
+  end
+  s = remove_wait_tag(s)
+  return s
+end
+
+local function transform_done(ln, is_task)
+  local s = ln
+  if is_task then
+    s, _ = string.gsub(s, '%[ %]', '[x]', 1)
+  else
+    s, _ = string.gsub(s, '^(%s*%-)(.*)', '%1 [x]%2', 1)
+  end
+  s = remove_wait_tag(s)
+  return s
+end
+
+local function transform_clear(ln, is_task)
+  local s = ln
+  if is_task then
+    s, _ = string.gsub(s, '%[.%] ', '', 1)
+  end
+  s = string.gsub(s, ':wait:', '', 1)
+  return s
+end
+
+local function transform_wait(ln, is_task)
+  local s = transform_todo(ln, is_task)
+  if not string.find(ln, ':wait:') then
+    s = s .. ' :wait:'
+  end
+  return s
+end
+
 local function mark_todo()
-  mark_impl(function(ln)
-    local s, _ = string.gsub(ln, '%[x%]', '[ ]', 1)
-    return s
-  end)
+  mark_impl(transform_todo)
 end
 
 local function mark_done()
-  mark_impl(function(ln)
-    local s, _ = string.gsub(ln, '%[ %]', '[x]', 1)
-    return s
-  end)
+  mark_impl(transform_done)
+end
+
+local function mark_clear()
+  mark_impl(transform_clear)
+end
+
+local function mark_wait()
+  mark_impl(transform_wait)
 end
 
 vim.keymap.set({ 'n' }, '<tab>', 'za', { desc = 'Toggle fold', silent = true, buffer = true })
@@ -125,5 +175,7 @@ vim.keymap.set({ 'n' }, '<<', dedent, { desc = 'Smart dedent', silent = true, bu
 -- vim.keymap.set({ 'i' }, '<tab>', indent, { desc = 'Smart indent', silent = true, buffer = true })
 -- vim.keymap.set({ 'i' }, '<s-tab>', dedent, { desc = 'Smart dedent', silent = true, buffer = true })
 vim.keymap.set({ 'n', 'i' }, '<c-cr>', insert_siblling, { desc = 'insert deeper heading', silent = true, buffer = true })
-vim.keymap.set({ 'n' }, '<leader>mtd', mark_done, { desc = 'Mark done', silent = true, buffer = true })
 vim.keymap.set({ 'n' }, '<leader>mtt', mark_todo, { desc = 'Mark done', silent = true, buffer = true })
+vim.keymap.set({ 'n' }, '<leader>mtd', mark_done, { desc = 'Mark done', silent = true, buffer = true })
+vim.keymap.set({ 'n' }, '<leader>mt ', mark_clear, { desc = 'Mark done', silent = true, buffer = true })
+vim.keymap.set({ 'n' }, '<leader>mtw', mark_wait, { desc = 'Mark done', silent = true, buffer = true })
