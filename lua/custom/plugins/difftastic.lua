f148ea709bf3fc87886c2a57899573c2d0e8d256
Hugh Crail <hughcrail@gmail.com>
2026-02-01 14:53:58 +0000

    keybinding for current jj revision

--- lua/custom/plugins/difftastic.lua
+++ lua/custom/plugins/difftastic.lua
@@ -1,7 +1,16 @@
 return {
-    "clabby/difftastic.nvim",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    config = function()
-        require("difftastic-nvim").setup()
-    end,
+  'clabby/difftastic.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  config = function()
+    require('difftastic-nvim').setup()
+  end,
+  keys = {
+    {
+      '<leader>jd',
+      function()
+        require('difftastic-nvim').open '@'
+      end,
+      desc = 'Open diff of current jj revision',
+    },
+  },
 }
